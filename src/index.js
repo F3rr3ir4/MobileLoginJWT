fb9cf1306bac7688c3da6f320e6cc7c750310b64
Leandro Ferreira Netto <leandroferreira757@msn.com>
2020-01-29 20:28:21 -0300

    create index

---
+++ src/index.js
@@ -0,0 +1,10 @@
+/******************************************
+ *  Author : Leandro Ferreira Netto
+ *  Created On : Wed Jan 29 2020
+ *  File : index.js
+ *******************************************/
+
+import React from "react";
+import { View } from "react-native";
+
+import { SigedOutRoutes, SigedInRoutes } from "./routes";
